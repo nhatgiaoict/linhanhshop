3e6ff9fc2a489f547f42fe336c3c7c1c02a20433
DESKTOP-3IJIOQ8\atlanticgroup <nhatgiaoict@gmail.com>
2017-09-11 01:08:54 +0700

    lession11: routing in angular 2

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import {mainRoutes} from './main.routes';
+import {Router, RouterModule} from '@angular/router';
+import {UserModule} from './user/user.module';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule,
+    RouterModule.forChild(mainRoutes)
   ],
   declarations: [MainComponent, ProductCategoryComponent]
 })
