8f04d70cf0e545b009fdc29c050f354a89470657
DESKTOP-3IJIOQ8\atlanticgroup <nhatgiaoict@gmail.com>
2017-09-30 22:46:43 +0700

    lession25:paging user

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 const userRoutes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
@@ -10,8 +15,12 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    PaginationModule,
+    FormsModule,
+    ModalModule.forRoot(),
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [DataService, NotificationService]
 })
 export class UserModule { }
