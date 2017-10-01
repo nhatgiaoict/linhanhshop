f36ca0e34484e795d8eb1e37d95cce0dadf73498
DESKTOP-3IJIOQ8\atlanticgroup <nhatgiaoict@gmail.com>
2017-10-01 11:04:33 +0700

    lession27:update user

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -5,14 +5,13 @@ import { NotificationService } from '../../core/services/notification.service';
 import { MessageConstants } from '../../core/common/message.constants';
 
 import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
-
+declare var moment:any;
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
 
   @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
 
@@ -61,6 +60,10 @@ export class UserComponent implements OnInit {
     this._dataService.get('/api/appUser/detail/' + id)
       .subscribe((response: any) => {
         this.entity = response;
+        for(let role of this.entity.Roles){
+          this.myRoles.push(role);
+        }
+        this.entity.BirthDay = moment(new Date(this.entity.BirthDay)).format('DD/MM/YYYY');
       });
   }
 
