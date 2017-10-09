5fbde65ee9b6b79d3928e5e71234c89633ab6b8d
DESKTOP-3IJIOQ8\atlanticgroup <nhatgiaoict@gmail.com>
2017-10-09 23:16:34 +0700

    lession30: assign permission to roles

--- src/app/main/function/function.component.ts
+++ src/app/main/function/function.component.ts
@@ -13,17 +13,20 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 })
 export class FunctionComponent implements OnInit {
   @ViewChild('addEditModal') public addEditModal: ModalDirective;
+  @ViewChild('permissionModal') public permissionModal: ModalDirective;
   @ViewChild(TreeComponent)
   private treeFunction: TreeComponent;
 
   public _functionHierachy: any[];
   public _functions: any[];
   public entity: any;
+  public functionId: string;
+  public _permission: any[];
   public editFlag: boolean;
   public filter: string = '';
   constructor(private _dataService: DataService,
-    private notificationService: NotificationService,
-    private utilityService: UtilityService) { }
+    private _notificationService: NotificationService,
+    private _utilityService: UtilityService) { }
 
   ngOnInit() {
     this.search();
@@ -35,12 +38,19 @@ export class FunctionComponent implements OnInit {
     this.addEditModal.show();
     this.editFlag = false;
   }
+  public showPermission(id: any) {
+    this._dataService.get('/api/appRole/getAllPermission?functionId=' + id).subscribe((response: any[]) => {
+      this.functionId = id;
+      this._permission = response;
+      this.permissionModal.show();
+    }, error => this._dataService.handleError(error));
+  }
   //Load data
   public search() {
     this._dataService.get('/api/function/getall?filter=' + this.filter)
       .subscribe((response: any[]) => {
         this._functions = response.filter(x => x.ParentId == null);
-        this._functionHierachy = this.utilityService.Unflatten(response);
+        this._functionHierachy = this._utilityService.Unflatten(response);
       }, error => this._dataService.handleError(error));
   }
 
@@ -51,19 +61,35 @@ export class FunctionComponent implements OnInit {
         this._dataService.post('/api/function/add', JSON.stringify(this.entity)).subscribe((response: any) => {
           this.search();
           this.addEditModal.hide();
-          this.notificationService.printSuccessMessage(MessageConstants.CREATED_OK_MSG);
+          this._notificationService.printSuccessMessage(MessageConstants.CREATED_OK_MSG);
         }, error => this._dataService.handleError(error));
       }
       else {
         this._dataService.put('/api/function/update', JSON.stringify(this.entity)).subscribe((response: any) => {
           this.search();
           this.addEditModal.hide();
-          this.notificationService.printSuccessMessage(MessageConstants.UPDATE_OK_MSG);
+          this._notificationService.printSuccessMessage(MessageConstants.UPDATE_OK_MSG);
         }, error => this._dataService.handleError(error));
 
       }
     }
 
+  }
+
+  //Save permission
+
+  public savePermission(valid:boolean, _permission:any[]){
+    if(valid){
+      var data={
+        Permissions:this._permission,
+        FunctionId:this.functionId
+      };
+      this._dataService.post('/api/appRole/savePermission', JSON.stringify(data))
+      .subscribe((response:any)=>{
+        this._notificationService.printSuccessMessage(response);
+        this.permissionModal.hide();
+      }, error=>this._dataService.handleError(error));
+    }
   }
   //Show edit form
   public showEdit(id: string) {
@@ -77,12 +103,12 @@ export class FunctionComponent implements OnInit {
   //Action delete
   public deleteConfirm(id: string): void {
     this._dataService.delete('/api/function/delete', 'id', id).subscribe((response: any) => {
-      this.notificationService.printSuccessMessage(MessageConstants.DELETE_OK_MSG);
+      this._notificationService.printSuccessMessage(MessageConstants.DELETE_OK_MSG);
       this.search();
     }, error => this._dataService.handleError(error));
   }
   //Click button delete turn on confirm
   public delete(id: string) {
-    this.notificationService.printConfirmationDialog(MessageConstants.CONFIRM_DELETE_MSG, () => this.deleteConfirm(id));
+    this._notificationService.printConfirmationDialog(MessageConstants.CONFIRM_DELETE_MSG, () => this.deleteConfirm(id));
   }
 }
